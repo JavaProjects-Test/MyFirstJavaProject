fc9335659cc580d0bfc75d0883be3b02e387352a
akum25 <akum25@bosAkum2541468.sapient.com>
2015-09-05 11:43:18 -0400

    New changes for Registration

---
+++ src/main/java/com/abcbank/dao/RegistrationDAO.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.abcbank.dao;
+
+/**
+ * @author akum25
+ *
+ */
+public class RegistrationDAO {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
